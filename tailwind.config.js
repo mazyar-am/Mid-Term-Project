cead548b97445bab13886b426a59dba742d207d7
Mazyar <mazyar.am97@gmail.com>
2025-05-01 16:22:09 +0330

    cutomized tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+
+      colors: {
+        'primary': ["#6D1600"]
+        ,
+        'secondary': ["#282A3A"]
+        ,
+        'border': ["#D9D9D9"]
+
+      }
+    }
+  }
+}
